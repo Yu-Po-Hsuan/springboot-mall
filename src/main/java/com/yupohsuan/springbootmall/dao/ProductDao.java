6629473e75914053d2c7f766859b5201d23c66ad
yupohsuan <yph3542@gmail.com>
2022-09-21 21:09:25 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/yupohsuan/springbootmall/dao/ProductDao.java
+++ src/main/java/com/yupohsuan/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.yupohsuan.springbootmall.dao;
 
+import com.yupohsuan.springbootmall.dto.ProductRequest;
 import com.yupohsuan.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/yupohsuan/springbootmall/service/ProductService.java
+++ src/main/java/com/yupohsuan/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.yupohsuan.springbootmall.service;
 
+import com.yupohsuan.springbootmall.dto.ProductRequest;
 import com.yupohsuan.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
